e8183852978a722b2c2560b91cb5825dc95a94c2
Swift Funcional <info@swiftfuncional.com>
2017-05-21 12:01:22 +0200

    Add getValue function, decide logger type using a switch

--- Funciones de orden superior.playground/Contents.swift
+++ Funciones de orden superior.playground/Contents.swift
@@ -9,3 +9,24 @@ func logToFile(_ msg: String) {
 func logToWindow(_ msg: String) {
     print(msg)
 }
+
+func getValue(from number: Int, loggerType: Int) -> String {
+    //First determine the result
+    
+    let result = "Something \(number)"
+    
+    switch loggerType {
+    case 1:
+        logToConsole(result)
+    case 2:
+        logToWindow(result)
+    case 3:
+        logToFile(result)
+    default:
+        break
+    }
+    
+    return result
+}
+
+getValue(from: 5, loggerType: 1)
