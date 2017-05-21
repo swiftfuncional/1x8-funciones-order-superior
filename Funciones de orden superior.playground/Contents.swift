e942d6d04db34d452e31caab424cf3fd790d4952
Swift Funcional <info@swiftfuncional.com>
2017-05-21 12:15:10 +0200

    Abstract from the logger type following the FP way

--- Funciones de orden superior.playground/Contents.swift
+++ Funciones de orden superior.playground/Contents.swift
@@ -10,36 +10,14 @@ func logToWindow(_ msg: String) {
     print(msg)
 }
 
-protocol Logger {
-    func print(_ msg: String)
-}
-
-class ConsoleLogger: Logger {
-    func print(_ msg: String) {
-        logToConsole(msg)
-    }
-}
-
-class WindowLogger: Logger {
-    func print(_ msg: String) {
-        logToWindow(msg)
-    }
-}
-
-class FileLogger: Logger {
-    func print(_ msg: String) {
-        logToFile(msg)
-    }
-}
-
-func getValue(from number: Int, logger: Logger) -> String {
+func getValue(from number: Int, logger: (String) -> Void) -> String {
     //First determine the result
     
     let result = "Something \(number)"
     
-    logger.print(result)
+    logger(result)
     
     return result
 }
 
-getValue(from: 5, logger: ConsoleLogger())
+getValue(from: 5, logger: logToConsole)
